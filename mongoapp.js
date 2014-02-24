f4f71c60a40b8f8fb02733ed69a04e9bcf871a8f
Marcin Kumorek <kumeek@gmail.com>
2014-02-24 22:37:13 +0000

    Update mongoapp.js with proper url and change db.collection

--- mongoapp.js
+++ mongoapp.js
@@ -1,12 +1,11 @@
 var MongoClient = require('mongodb').MongoClient
 
 // Open the connection to the server
-MongoClient.connect('mongodb://localhost:27017/test', function (err, db) {
+MongoClient.connect('mongodb://127.0.0.1:27017/test', function (err, db) {
 	if (err) throw err
 
 	// Find one document in our collection
-	db.collection.findOne({}, function (err, doc) {
-		if (err) throw err
+	db.collection('users').findOne({}, function (err, doc) {
 		// Print the result
 		console.dir(doc)
 
