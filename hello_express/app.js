4eebeaa08392b34c39bea9e3e7e2fac965b9bc7f
Marcin Kumorek <kumeek@gmail.com>
2014-02-24 23:16:06 +0000

    Update hello_express app with swig template engine

--- hello_express/app.js
+++ hello_express/app.js
@@ -1,9 +1,14 @@
 var express = require('express')
 var port = 8080
 var app = express()
+var consolidate = require('consolidate')
+
+app.engine('html', cons.swig)
+app.set('view engine', 'html')
+app.set('views', __dirname + '/views')
 
 app.get('/', function (req, res) {
-	res.send('Hello from express!')
+	res.render('index', { 'name': 'Swig' })
 })
 
 app.get('*', function (req, res) {
