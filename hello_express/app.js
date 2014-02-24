843b1733620c9fe4c66328a3bae76311e8740b6b
Marcin Kumorek <kumeek@gmail.com>
2014-02-24 23:34:29 +0000

    Add mongodb to simple hello_express app

--- hello_express/app.js
+++ hello_express/app.js
@@ -2,18 +2,29 @@ var express = require('express')
 var port = 8080
 var app = express()
 var consolidate = require('consolidate')
+var MongoClient = require('mongodb').MongoClient
+var Server = require('mongodb').Server
 
 app.engine('html', consolidate.swig)
 app.set('view engine', 'html')
 app.set('views', __dirname + '/views')
 
+var mongoClient = new MongoClient(new Server('localhost', 27017, { 'native_parser': true }))
+var db = mongoClient.db('course')
+
 app.get('/', function (req, res) {
-	res.render('index', { 'name': 'Swig' })
+	db.collection('hello_mongon_express').findOne({}, function (err, doc) {
+		res.render('index', doc)
+	})
 })
 
 app.get('*', function (req, res) {
 	res.send('Sorry, the page does not exist!')
 })
 
-app.listen(port)
-console.log('Express app is listening on port: ' + port)
+mongoClient.open(function (err, mongoclient) {
+	if (err) throw err
+
+	app.listen(port)
+	console.log('Express app is listening on port: ' + port)
+})
