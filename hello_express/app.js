f66c1d789f73e08564b18a615e4d6d64992b9b40
Marcin Kumorek <kumeek@gmail.com>
2014-02-25 00:05:55 +0000

    Add errorHandler, show how to get params from url

--- hello_express/app.js
+++ hello_express/app.js
@@ -5,6 +5,14 @@ var consolidate = require('consolidate')
 var MongoClient = require('mongodb').MongoClient
 var Server = require('mongodb').Server
 
+// Handler for internal server errors
+function errorHandler (err, req, res, next) {
+	console.error(err.message)
+	console.error(err.stack)
+	res.status(500)
+	res.render('error_template', { error: err })
+}
+
 app.engine('html', consolidate.swig)
 app.set('view engine', 'html')
 app.set('views', __dirname + '/views')
@@ -12,10 +20,16 @@ app.set('views', __dirname + '/views')
 var mongoClient = new MongoClient(new Server('localhost', 27017, { 'native_parser': true }))
 var db = mongoClient.db('course')
 
+app.use(errorHandler)
 app.get('/', function (req, res) {
 	db.collection('users').findOne({}, function (err, doc) {
 		res.render('index', doc)
 	})
+})
+app.get('/:name', function (req, res, next) {
+	var name = req.params.name
+	var getVar = req.query.getVar
+	res.render('hello', { name: name, getVar: getVar })
 })
 
 app.get('*', function (req, res) {
