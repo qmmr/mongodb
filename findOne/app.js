bc2474c010abfd6a9eae8b3216578b455e14af82
Marcin Kumorek <kumeek@gmail.com>
2014-03-02 23:21:20 +0000

    Add findOne app

---
+++ findOne/app.js
@@ -0,0 +1,13 @@
+var MongoClient = require('mongodb').MongoClient
+
+// mongoimport -d course -c grades grades.json
+MongoClient.connect('mongodb://127.0.0.1:27017/course', function (err, db) {
+	if (err) throw err
+
+	db.collection('grades').findOne(query, function (err, doc) {
+		if (err) throw err
+		console.log('doc', doc)
+
+		db.close()
+	})
+})
