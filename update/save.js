05b4e32b3331817a6d996f41c7aa56234a062ed5
Marcin Kumorek <kumeek@gmail.com>
2014-03-09 10:48:55 +0000

    Update save.js to find first item

--- update/save.js
+++ update/save.js
@@ -3,7 +3,7 @@ var MongoClient = require('mongodb').MongoClient
 MongoClient.connect('mongodb://127.0.0.1:27017/course', function (err, db) {
 	if (err) throw err
 
-	var query = { 'student': 'Steve', 'assignment': 'hw1' }
+	var query = { 'assignment': 'hw1' }
 
 	db.collection('grades').findOne(query, function (err, doc) {
 		if (err) throw err
