96308fc5f54289d2d329ed5b83c1866f7b8efe35
Marcin Kumorek <kumeek@gmail.com>
2014-03-09 10:46:59 +0000

    Add save.js

---
+++ update/save.js
@@ -0,0 +1,21 @@
+var MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('mongodb://127.0.0.1:27017/course', function (err, db) {
+	if (err) throw err
+
+	var query = { 'student': 'Steve', 'assignment': 'hw1' }
+
+	db.collection('grades').findOne(query, function (err, doc) {
+		if (err) throw err
+
+		doc['last_updated'] = new Date()
+
+		db.collection('grades').save(doc, function (err, doc) {
+			if (err) throw err
+
+			console.dir('Successfully saved ' + doc + ' document.')
+
+			return db.close()
+		})
+	})
+})
