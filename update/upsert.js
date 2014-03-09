5b0a4ae965e27255ff7230c23695ff0f60b5239b
Marcin Kumorek <kumeek@gmail.com>
2014-03-09 10:41:32 +0000

    Add $set method that replaces two params

--- update/upsert.js
+++ update/upsert.js
@@ -4,7 +4,8 @@ MongoClient.connect('mongodb://127.0.0.1:27017/course', function (err, db) {
 	if (err) throw err
 
 	var query = { 'student': 'Steve', 'assignment': 'hw1' }
-	var replacement = { 'student': 'Steve', 'assignment': 'hw1', 'grade': 75, 'last_updated': new Date() }
+	// var replacement = { 'student': 'Steve', 'assignment': 'hw1', 'grade': 75, 'last_updated': new Date() }
+	var replacement = { '$set': { 'grade': 75, 'last_updated': new Date() } }
 	var options = { 'upsert': 1 }
 
 	db.collection('grades').update(query, replacement, options, function (err, doc) {
