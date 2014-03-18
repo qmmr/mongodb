94f9facc98c85487777b943c25a916dbb446f3bd
Marcin Kumorek <kumeek@gmail.com>
2014-03-18 22:58:38 +0000

    Code style

--- remove/remove.js
+++ remove/remove.js
@@ -3,7 +3,7 @@ var MongoClient = require('mongodb').MongoClient
 MongoClient.connect('mongodb://127.0.0.1:27017/course', function (err, db) {
 	if (err) throw err
 
-	var query = {  'name': {'$regex' : '^stued' } }
+	var query = { 'name': {'$regex' : '^stued' } }
 
 	db.collection('users').remove(query, function (err, docs) {
 
