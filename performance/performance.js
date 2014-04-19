d7b795887f4566f0b12a604ec725d44cd91496fd
Marcin Kumorek <kumeek@gmail.com>
2014-04-19 12:15:13 +0100

    Explain why not to use queries with wrong hint

--- performance/performance.js
+++ performance/performance.js
@@ -11,8 +11,15 @@ MongoClient.connect('mongodb://127.0.0.1:27017/test', function (err, db) {
 	// but if we want to use natural query, we can give the options object
 	// with hint to use $natural: 1
 
-	var cursor = db.collection('coll').find({ 'x': 2 }, {}, { 'hint': { '$natural': 1 } })
+	// var cursor = db.collection('coll').find({ 'x': 2 }, {}, { 'hint': { '$natural': 1 } })
 	// and now the explain returns 'BasicCursor'
+
+	// there is always an index on _id
+	// !!! however !!!
+	// there is no benefit in using this, because we are querying for x and this
+	// has nothing to do with _id, so we need to scan all the documents
+	// what's even worse we need to scan the index first!
+	var cursor = db.collection('coll').find({ 'x': 2 }, {}, { 'hint': { '_id': 1 } })
 
 	cursor.explain(function (err, output) {
 		if (err) throw err
