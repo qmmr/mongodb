4e7881542c9f3d3487f2904c39b31ccebdbf24b5
Marcin Kumorek <kumeek@gmail.com>
2014-04-19 12:08:38 +0100

    Add explanation how to use indexes and hint

--- performance/performance.js
+++ performance/performance.js
@@ -3,7 +3,16 @@ var MongoClient = require('mongodb').MongoClient
 MongoClient.connect('mongodb://127.0.0.1:27017/test', function (err, db) {
 	if (err) throw err
 
-	var cursor = db.collection('coll').find({ 'x': 2 })
+	// to index the coll collection we run this query in the terminal
+	// db.coll.ensureIndex({x:1, y:1, z:1})
+
+	// var cursor = db.collection('coll').find({ 'x': 2 })
+	// the explain now returns 'BtreeCursor x_1_y_1_z_1'
+	// but if we want to use natural query, we can give the options object
+	// with hint to use $natural: 1
+
+	var cursor = db.collection('coll').find({ 'x': 2 }, {}, { 'hint': { '$natural': 1 } })
+	// and now the explain returns 'BasicCursor'
 
 	cursor.explain(function (err, output) {
 		if (err) throw err
