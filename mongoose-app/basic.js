a32941af2b56469360a3f3269e6d488fed529b10
Marcin Kumorek <kumeek@gmail.com>
2014-05-07 21:09:11 +0100

    Add mongoose-app with basic.js

---
+++ mongoose-app/basic.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose')
+var express = require('express')
+
+mongoose.connect('mongodb://localhost', function(err) {
+	if (err) throw err
+	console.log('connected to mongodb!')
+
+	var app = express()
+	app.get('/', function (req, resp) {
+		resp.send(200, 'hello from mongoose!')
+	})
+
+	app.listen(3000, function () {
+		console.log('App is listening! localhost:3000')
+	})
+})
