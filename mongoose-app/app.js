e62290a0f668d5cd8022574f80bd9b1a15a8a3ec
Marcin Kumorek <kumeek@gmail.com>
2014-05-22 23:01:38 +0100

    Add login/logout/signup to layout.jade

--- mongoose-app/app.js
+++ mongoose-app/app.js
@@ -4,8 +4,9 @@ var routes = require('./routes')
 
 mongoose.connect('mongodb://localhost', function ( err ) {
     if (err) throw err
-    var app = express()
-    routes(app)
+
+    var app = express();
+    routes(app);
 
     app.listen(3000, function ( req, resp ) {
         console.log('now listening on http://localhost:3000')
--- mongoose-app/routes/index.js
+++ mongoose-app/routes/index.js
@@ -1,8 +1,10 @@
 var errors = require('./errors')
+
 module.exports = function ( app ) {
+
     app.get('/', function ( req, resp ) {
         resp.render('home.jade')
-    })
+    });
 
     errors( app );
 }
