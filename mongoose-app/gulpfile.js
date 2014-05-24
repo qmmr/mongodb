c8a7a9efc62bc3bbbdaf4eeb9cf654928ff3bde2
Marcin Kumorek <kumeek@gmail.com>
2014-05-24 17:04:02 +0100

    Add gulpfile.js

---
+++ mongoose-app/gulpfile.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var gulp = require('gulp');
+var eslint = require('gulp-eslint');
+
+gulp.task('eslint', function () {
+    return gulp.src('*.js')
+        .pipe(eslint())
+        .pipe(eslint.format());
+});
+
+gulp.task('watch', function () {
+    gulp.watch('**/*.js', ['eslint']);
+});
+
+gulp.task('default', ['eslint']);
