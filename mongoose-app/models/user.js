84091e446bdd9c2a1379bbaa1a211ee21c0af8c7
Marcin Kumorek <kumeek@gmail.com>
2014-05-24 11:35:03 +0100

    Add helper methods to User model

--- mongoose-app/models/user.js
+++ mongoose-app/models/user.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var validEmail = require('../helpers/validate/email');
 
 var schema = new mongoose.Schema({
-    _id: { type: String, validate: validEmail },
+    _id: { type: String, lowercase: true, trime: true, validate: validEmail },
     name: { first: String, last: String },
     salt: { type: String, required: true },
     hash: { type: String, required: true },
