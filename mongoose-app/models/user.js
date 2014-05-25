776730b9e95865bb7998918f5764f8a3a736ed8c
Marcin Kumorek <kumeek@gmail.com>
2014-05-25 15:11:20 +0100

    Extract Schema from mongoose

--- mongoose-app/models/user.js
+++ mongoose-app/models/user.js
@@ -1,12 +1,15 @@
 'use strict';
 
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 var validEmail = require('../helpers/validate/email');
 
-mongoose.Schema({
+var User = new Schema({
     _id: { type: String, lowercase: true, trime: true, validate: validEmail },
     name: { first: String, last: String },
     salt: { type: String, required: true },
     hash: { type: String, required: true },
     created: { type: Date, default: Date.now } // optional index: true
 });
+
+mongoose.model('User', User);
