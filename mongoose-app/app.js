5a048191d0b3bb32ae29ae48c71e071c9383cd25
Marcin Kumorek <kumeek@gmail.com>
2014-05-25 15:16:34 +0100

    Add dependency of body-parser

--- mongoose-app/app.js
+++ mongoose-app/app.js
@@ -2,6 +2,7 @@
 
 var mongoose = require('mongoose');
 var express = require('express');
+var bodyParser = require('body-parser');
 var routes = require('./routes');
 var PORT = 8888;
 
@@ -9,9 +10,10 @@ mongoose.connect('mongodb://localhost', function(err) {
     if (err) throw err;
 
     var app = express();
+    app.use(bodyParser());
     routes(app);
 
-    app.listen(PORT, function(req, res) {
+    app.listen(PORT, function() {
         console.log('now listening on http://localhost:' + PORT);
     });
 });
