3bd1f7348ff265508fa570a5d418bc91cac233f3
Marcin Kumorek <kumeek@gmail.com>
2014-05-26 19:13:41 +0100

    Update app.js with new stuff from mongoose

--- mongoose-app/app.js
+++ mongoose-app/app.js
@@ -1,6 +1,8 @@
 'use strict';
 
+var path = require('path');
 var mongoose = require('mongoose');
+var db = mongoose.connection;
 var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
@@ -8,13 +10,20 @@ var session = require('express-session');
 var routes = require('./routes');
 var PORT = 8888;
 
-mongoose.connect('mongodb://localhost', function(err) {
+mongoose.connect('mongodb://localhost/bazinga');
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback(err) {
     if (err) throw err;
 
     var app = express();
+    app.set('views', path.join(__dirname, '/views'));
+    app.set('view engine', 'jade');
+    // app.engine('jade', require('jade').__express);
+
     app.use(bodyParser());
     app.use(cookieParser('mongooseApp'));
     app.use(session());
+
     routes(app);
 
     app.listen(PORT, function() {
