12e50dc7fbb2a971c4062d174cafcfbea389e54b
Marcin Kumorek <kumeek@gmail.com>
2014-05-26 20:39:32 +0100

    Add BlogPost model

---
+++ mongoose-app/models/post.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var blogSchema = mongoose.schema({
+    title: { type: String, trime: true },
+    body: { type: String },
+    author: { type: String, ref: 'User' },
+    createdOn: { type: Date, default: Date.now }
+});
+
+// implement later on real tweet via mongoose-lifecycle plugin
+var lifecycle = require('mongoose-lifecycle');
+blogSchema.plugin( lifecycle );
+
+var Post = mongoose.model( 'BlogPost', blogSchema );
+// handle events
+Post.on('afterInsert', function ( post ) {
+    // fake tweet this
+    var url = 'http://localhost:8888/posts/';
+    console.log('Read my new blog post! %s%s', url, post.id);
+});
