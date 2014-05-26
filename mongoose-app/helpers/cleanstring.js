f76b3f253b3e34db943dd1217c3a51fc285fb2fa
Marcin Kumorek <kumeek@gmail.com>
2014-05-26 19:09:41 +0100

    Cleanup helpers

--- mongoose-app/helpers/cleanstring.js
+++ mongoose-app/helpers/cleanstring.js
@@ -1,5 +1,5 @@
 'use strict';
 
-module.exports = function(str) {
+module.exports = function ( str ) {
     return str.trim().toLowerCase();
 };
--- mongoose-app/helpers/validate/email.js
+++ mongoose-app/helpers/validate/email.js
@@ -1,6 +1,6 @@
 'use strict';
 
 module.exports = function ( email ) {
-    var re = /[a-z0-9-_.]+@[a-z0-9]\.[a-z0-9]{3,4}$/i;
+    var re = /[a-z0-9-_.]+@[a-z0-9-_.]+\.[a-z0-9-_.]{2,3}/i;
     return re.test( email );
 };
