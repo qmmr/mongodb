b45eda149720e31a77c15b7f09e78e153a375cf6
Marcin Kumorek <kumeek@gmail.com>
2014-05-26 21:54:40 +0100

    Implement find post by id

--- mongoose-app/routes/posts.js
+++ mongoose-app/routes/posts.js
@@ -1,8 +1,24 @@
 'use strict';
 
 var loggedIn = require('../middleware/islogged');
+var mongoose = require('mongoose');
+require('../models/post');
+var BlogPost = mongoose.model('BlogPost');
 
-module.exports = function( app ) {
+module.exports = function ( app ) {
+    app.route('/post/:id')
+        .get(function ( req, res, next ) {
+            var query = BlogPost.findById( req.param('id') );
+            // runs additional query to find the author of the post
+            query.populate( 'author' );
+            // run the query
+            query.exec(function ( err, post ) {
+                if ( err ) return next( err );
+                if ( !post ) return next(); // 404
+                res.render( 'post/view', { post: post } );
+            });
+        });
+
     app.route('/post')
         .get(loggedIn, function ( req, res ) {
             res.render('posts/create');
