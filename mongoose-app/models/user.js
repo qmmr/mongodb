734eccb7d58d45f46ac9d62299431094f90c7627
Marcin Kumorek <kumeek@gmail.com>
2014-05-26 19:09:59 +0100

    Fix typo

--- mongoose-app/models/user.js
+++ mongoose-app/models/user.js
@@ -5,7 +5,7 @@ var Schema = mongoose.Schema;
 var validEmail = require('../helpers/validate/email');
 
 var User = new Schema({
-    _id: { type: String, lowercase: true, trime: true, validate: validEmail },
+    _id: { type: String, lowercase: true, trim: true, validate: validEmail },
     name: { first: String, last: String },
     salt: { type: String, required: true },
     hash: { type: String, required: true },
