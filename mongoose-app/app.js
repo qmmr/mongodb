c7e83ccbd2ebd0b5ebce7a98e5e15522a8be7f6e
Marcin Kumorek <kumeek@gmail.com>
2014-05-27 18:23:05 +0100

    Update requires for app.js, add express-mongoose and static method -> edit

--- mongoose-app/app.js
+++ mongoose-app/app.js
@@ -2,14 +2,21 @@
 
 var path = require('path');
 var mongoose = require('mongoose');
+// add mongoose query and promise support to express
+require('express-mongoose');
+
+// var models = require('./models');
+var routes = require('./routes');
+require('./middleware');
+
 var db = mongoose.connection;
 var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
-var routes = require('./routes');
 var PORT = 8888;
 
+mongoose.set('debug', true);
 mongoose.connect('mongodb://localhost/bazinga');
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback(err) {
--- mongoose-app/models/post.js
+++ mongoose-app/models/post.js
@@ -8,6 +8,25 @@ var blogSchema = mongoose.Schema({
     createdOn: { type: Date, default: Date.now }
 });
 
+blogSchema.statics.edit = function ( req, cb ) {
+    var id = req.param( 'id' );
+    var author = req.session.user;
+
+    // validate that current user authored the post
+    var query = { _id: id, author: author };
+    var update = {
+        title: req.param( 'title' ),
+        body: req.param( 'body' )
+    };
+
+    this.update(query, update, function ( err, updatedDoc ) {
+        if ( err ) return cb( err );
+        if ( updatedDoc === 0 )
+            return cb( new Error('no post to modify!') );
+        cb();
+    });
+};
+
 // implement later on real tweet via mongoose-lifecycle plugin
 var lifecycle = require('mongoose-lifecycle');
 blogSchema.plugin( lifecycle );
